352175ea18bf44bbec37ef6751a5474cdf8dab01
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-05-07 16:35:57 +0500

    udpate the json file

--- drug_network_up11.js
+++ drug_network_up11.js
@@ -7,7 +7,7 @@ $(function () {
 
 
 var json_GeneralFile = "json/json_GeneralFile.json";
-var json_GeneralFile = "json/json2.json";
+var json_GeneralFile = "json/json5.json";
 var json_drugData = "json/json_drugData.json";
 var json_proteinData = "json/json_proteinData.json";
 var json_interactionData = "json/json_interactionData.json";
