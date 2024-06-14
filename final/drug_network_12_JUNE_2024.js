eaff77ecbb2b6e5c52dddd21e1c708bccefcb307
Aamir Yasin <100421401+aamir910@users.noreply.github.com>
2024-06-14 12:27:29 +0500

    added the tooltip click functionality

--- final/drug_network_12_JUNE_2024.js
+++ final/drug_network_12_JUNE_2024.js
@@ -6,30 +6,30 @@ $(function () {
   
   //Pass jsonFiles Here
   
-  // var json_GeneralFile = "json/json_GeneralFile.json";
-  // var json_GeneralFile = "json/json5.json";
-  // var json_drugData = "json/json_drugData.json";
-  // var json_proteinData = "json/json_proteinData.json";
-  // var json_interactionData = "json/json_interactionData.json";
+  var json_GeneralFile = "json/json_GeneralFile.json";
+  var json_GeneralFile = "json/json5.json";
+  var json_drugData = "json/json_drugData.json";
+  var json_proteinData = "json/json_proteinData.json";
+  var json_interactionData = "json/json_interactionData.json";
   
-  var json_GeneralFile = "/static/json-sample/json_GeneralFile.json";
-  var json_drugData = "/static/json-sample/json_drugData.json";
-  var json_proteinData = "/static/json-sample/json_proteinData.json";
-  var json_interactionData = "/static/json-sample/json_interactionData.json"
+  // var json_GeneralFile = "/static/json-sample/json_GeneralFile.json";
+  // var json_drugData = "/static/json-sample/json_drugData.json";
+  // var json_proteinData = "/static/json-sample/json_proteinData.json";
+  // var json_interactionData = "/static/json-sample/json_interactionData.json"
   
-  if (drug_bank_ids) {
-      json_GeneralFile = "/drugs_network/general_data?drug_bank_ids=" + drug_bank_ids.join(',');
-      json_drugData = "/drugs_network/drug_data?drug_bank_ids=" + drug_bank_ids.join(',');
-      json_proteinData = "/drugs_network/protein_data?drug_bank_ids=" + drug_bank_ids.join(',');
-      json_interactionData = "/drugs_network/interaction_data?drug_bank_ids=" + drug_bank_ids.join(',');
-  }
+  // if (drug_bank_ids) {
+  //     json_GeneralFile = "/drugs_network/general_data?drug_bank_ids=" + drug_bank_ids.join(',');
+  //     json_drugData = "/drugs_network/drug_data?drug_bank_ids=" + drug_bank_ids.join(',');
+  //     json_proteinData = "/drugs_network/protein_data?drug_bank_ids=" + drug_bank_ids.join(',');
+  //     json_interactionData = "/drugs_network/interaction_data?drug_bank_ids=" + drug_bank_ids.join(',');
+  // }
   
-  if (drug_bank_id) {
-      json_GeneralFile = "/drug_network/" + drug_bank_id + "/general_data";
-      json_drugData = "/drug_network/" + drug_bank_id + "/drug_data";
-      json_proteinData = "/drug_network/" + drug_bank_id + "/protein_data";
-      json_interactionData = "/drug_network/" + drug_bank_id + "/interaction_data";
-  }
+  // if (drug_bank_id) {
+  //     json_GeneralFile = "/drug_network/" + drug_bank_id + "/general_data";
+  //     json_drugData = "/drug_network/" + drug_bank_id + "/drug_data";
+  //     json_proteinData = "/drug_network/" + drug_bank_id + "/protein_data";
+  //     json_interactionData = "/drug_network/" + drug_bank_id + "/interaction_data";
+  // }
   
   // code to get the li of the network visualization
   
@@ -133,8 +133,8 @@ $(function () {
   }
   
   window.onload = function () {
-    readDrugJSON();
-    // processData(numberofnodes, slicedata);
+    // readDrugJSON();
+    processData(numberofnodes, slicedata);
     // getDrugJsonData(drugBankId);
   };
   
@@ -2062,7 +2062,7 @@ $(function () {
     node
       .filter((node) => node.child_type === "disease_type")
       .on("click", function (event, d) {
-        window.open(`https://clinicaltrials.gov/search?cond=${d.id}`, "_blank");
+        // window.open(`https://clinicaltrials.gov/search?cond=${d.id}`, "_blank");
         showDialog(d.id, d.id);
       })
       // Attach cursor style change on mouseover to all nodes
@@ -2087,18 +2087,18 @@ $(function () {
         tooltip2
           .style("left", event.pageX + 20 + "px")
           .style("top", event.pageY + 20 + "px")
-          .html(d.id); // Set HTML content before handling click event
-      })
-      .on("mouseout", function () {
-        tooltip2.transition().style("opacity", 0);
-      });
-    
-    tooltip2.on("click", function (event, d) {
-      // This function will execute when the tooltip is clicked
-      // Note: 'd' might not be defined here, you may need to handle this case
-      window.open(`https://clinicaltrials.gov/search?cond=${d.id}`, "_blank");
-    });
+          .html(`
+            <div>
+              ${d.id}<br>
+            
+              <a href="https://clinicaltrials.gov/search?cond=${d.id}" target="_blank">Click here</a>
+            </div>
+          `); // Set HTML content with link before handling click event
   
+    
+      })
+      
+   
     node
       .filter(function (d) {
         return d.isParent;
